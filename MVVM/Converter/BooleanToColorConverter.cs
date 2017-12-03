cc3bef8da96e3ddf45ce86c256b8fc368237584c
DawidSarnecki <test@solwit.com>
2017-12-04 00:05:47 +0100

    Added simple converter.

---
+++ MVVM/Converter/BooleanToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MVVM.Converter
+{
+    class BooleanToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool actualFlag = (bool)value;
+            return actualFlag ? Brushes.DarkBlue : Brushes.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
